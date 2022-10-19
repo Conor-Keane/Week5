478b439fc27ad5c3fa258d25fa9ac5413b143137
Conor-Keane <g00391846@atu.ie>
2022-10-19 20:18:29 +0100

    Added an add feature to the calculator

--- src/main/java/ie/atu/Calculator.java
+++ src/main/java/ie/atu/Calculator.java
@@ -6,4 +6,11 @@ public class Calculator {
     public Calculator() {
         calcValue = 0;
     }
+
+    public int add() {
+        int firstNum = 4;
+        int secondNum = 2;
+        calcValue = firstNum + secondNum;
+        return calcValue;
+    }
 }
