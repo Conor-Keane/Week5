8b51481f619476a16e07958e87260925240daba2
Conor-Keane <g00391846@atu.ie>
2022-10-19 19:55:22 +0100

    Added Test class

---
+++ src/test/java/ie/atu/CalculatorTest.java
@@ -0,0 +1,16 @@
+package ie.atu;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+
+    }
+}
