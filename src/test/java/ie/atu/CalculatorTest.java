49963b5a2a9aa9498f2f5bb4812039defd566cc4
Conor-Keane <g00391846@atu.ie>
2022-10-19 20:05:56 +0100

    Added Test add class

--- src/test/java/ie/atu/CalculatorTest.java
+++ src/test/java/ie/atu/CalculatorTest.java
@@ -2,11 +2,18 @@ package ie.atu;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
     @BeforeEach
     void setUp() {
+    }
+
+    @Test
+    void testAdd() {
+        assertEquals(6, myCalc.add());
     }
 
     @AfterEach
